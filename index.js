6e4235c9f726bdf4945d1884e4d32984c13b7f4c
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-08-19 20:48:14 +0300

    fixed an issue with index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
 const assetLinks = [
   {
     relation: ["delegate_permission/common.handle_all_urls"],
@@ -11,12 +10,11 @@ const assetLinks = [
       namespace: "android_app",
       package_name: "com.cairosquad.movio",
       sha256_cert_fingerprints: [
-        "C6:48:DD:3A:AA:90:99:34:36:56:0F:E5:33:EE:2C:2D:11:76:C6:48:AC:27:C4:11:24:07:C2:3D:AB:C5:00:97"
-      ]
-    }
-  }
+        "C6:48:DD:3A:AA:90:99:34:36:56:0F:E5:33:EE:2C:2D:11:76:C6:48:AC:27:C4:11:24:07:C2:3D:AB:C5:00:97",
+      ],
+    },
+  },
 ];
-
 
 app.get("/.well-known/assetlinks.json", (req, res) => {
   res.json(assetLinks);
